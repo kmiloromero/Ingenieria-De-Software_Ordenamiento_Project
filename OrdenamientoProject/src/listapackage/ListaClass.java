348b0ca6f97b6592dccca0dc03f44fbab7c4322b
kmiloromero <scamiloromerol@gmail.com>
2021-09-05 18:02:22 -0500

    Update ListaClass.java

--- OrdenamientoProject/src/listapackage/ListaClass.java
+++ OrdenamientoProject/src/listapackage/ListaClass.java
@@ -10,79 +10,84 @@ package listapackage;
  * @author Romero
  */
 public class ListaClass {
-    
-/* se crea el metodo Nodo que será la estructura de la lista con los atributos
+
+    /* se crea el metodo Nodo que será la estructura de la lista con los atributos
  * int dato que almacenara el valor numerico y apuntador que direcciona al   
  * siguiente nodo.
- */   
-    class Nodo{
-    int dato;
-    Nodo siguiente;
+     */
+    class Nodo {
+
+        int dato;
+        Nodo siguiente;
     }
-/*se crea un apuntador externo, el cual tendra la direcion del primer nodo*/
+    /*se crea un apuntador externo, el cual tendra la direcion del primer nodo*/
     private Nodo raiz = null;
-    
-/* se crea el metodo insertar,que trae por parametro un Long numeroel cual 
+
+    /* se crea el metodo insertar,que trae por parametro un Long numeroel cual 
  * siempre que se inserta un nodo, buscara la posicion en la lista donde 
  * debe quedar para asi buscar su ordenamiento en el momento de la insercion
- */
-    public void InsercionAscendente(double numero){
+     */
+    public void InsercionAscendente(double numero) {
         Nodo nuevonodo = new Nodo();
-    //guardamos la informacion entrante
-        nuevonodo.dato = (int)numero;
-        if(raiz == null){
+        //guardamos la informacion entrante
+        nuevonodo.dato = (int) numero;
+        if (raiz == null) {
             raiz = nuevonodo;
-        } else{
-            if(numero < raiz.dato){
+            nuevonodo.siguiente = null;
+        } else {
+            if (numero < raiz.dato) {
                 nuevonodo.siguiente = raiz;
                 raiz = nuevonodo;
-            } else{
+            } else {
                 Nodo reco = raiz;
-                Nodo atras=raiz;
-                while(numero >= reco.dato && reco.siguiente != null ){
+                Nodo atras = raiz;
+                while (numero >= reco.dato && reco.siguiente != null) {
                     atras = reco;
-                    reco = reco.siguiente;                
-                } 
-                if(numero >= reco.dato){
-                    reco.siguiente = nuevonodo;                
-                } else{
-                    nuevonodo.siguiente = reco;
-                    atras.siguiente = nuevonodo;
+                    reco = reco.siguiente;
                 }
-            }
-        }      
-    }
-    
-    public void InsercionDescendente(double numero){
-            Nodo nuevonodo = new Nodo();
-    //guardamos la informacion entrante
-        nuevonodo.dato = (int)numero;
-        if(raiz == null){
-            raiz = nuevonodo;
-        } else{
-            if(numero > raiz.dato){
-                nuevonodo.siguiente = raiz;
-                raiz = nuevonodo;
-            } else{
-                Nodo reco = raiz;
-                Nodo atras=raiz;
-                while(numero >= reco.dato && reco.siguiente != null ){
-                    atras = reco;
-                    reco = reco.siguiente;                
-                } 
-                if(numero <= reco.dato){
-                    reco.siguiente = nuevonodo;                
-                } else{
+                if (numero >= reco.dato) {
+                    reco.siguiente = nuevonodo;
+                    nuevonodo.siguiente = null;
+                } else {
                     nuevonodo.siguiente = reco;
                     atras.siguiente = nuevonodo;
                 }
             }
         }
     }
-    
+
+    public void InsercionDescendente(double numero) {
+        Nodo nuevonodo = new Nodo();
+        //guardamos la informacion entrante
+        nuevonodo.dato = (int) numero;
+        if (raiz == null) {
+            raiz = nuevonodo;
+            nuevonodo.siguiente = null;
+        } else {
+            if (numero > raiz.dato) {
+                nuevonodo.siguiente = raiz;
+                raiz = nuevonodo;
+            } else {
+                Nodo reco = raiz;
+                Nodo atras = raiz;
+                while (numero >= reco.dato && reco.siguiente != null) {
+                    atras = reco;
+                    reco = reco.siguiente;
+                }
+                if (numero <= reco.dato) {
+                    reco.siguiente = nuevonodo;
+                    nuevonodo.siguiente = null;
+                } else {
+                    nuevonodo.siguiente = reco;
+                    atras.siguiente = nuevonodo;
+                }
+            }
+        }
+    }
+
     public void imprimir(){
         Nodo reco = raiz;
-        while(reco != null){
+        while(reco.siguiente != null){
             System.out.println(reco.dato + " - ");
             reco = reco.siguiente;            
         }
