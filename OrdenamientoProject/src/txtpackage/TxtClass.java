5181a622f26dc7fa93cd9448ea46662273705e15
kmiloromero <scamiloromerol@gmail.com>
2021-09-06 11:40:01 -0500

    Commit
    --

--- OrdenamientoProject/src/txtpackage/TxtClass.java
+++ OrdenamientoProject/src/txtpackage/TxtClass.java
@@ -4,6 +4,15 @@
  * and open the template in the editor.
  */
 package txtpackage;
+import listapackage.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -11,4 +20,42 @@ package txtpackage;
  */
 public class TxtClass {
     
+    private String path = "C:\\Users\\secar\\Downloads\\prueba.txt";
+    private ListaClass b;
+
+    public void LecturaTxt(){
+ 
+        File archivo = null;
+        FileReader fr = null;
+        FileWriter wr = null;
+        PrintWriter pw = null;
+        
+    try {
+         // Lectura de Fichero con Filereader
+         archivo = new File (path);
+         fr = new FileReader (archivo);
+         
+         ArrayList<String> lista = new ArrayList();
+         //Escritura de Fichero
+         
+         
+         
+         
+         
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+   }
 }
