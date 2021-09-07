7ff440db12c67ef0079830da1fb0c9a24bffb0c4
kmiloromero <scamiloromerol@gmail.com>
2021-09-06 22:56:41 -0500

    Rediseño MVC

--- OrdenamientoProject/src/Models/TxtClass.java
+++ OrdenamientoProject/src/Models/TxtClass.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package txtpackage;
-import listapackage.*;
+package Models;
 
 import java.io.BufferedReader;
 import java.io.File;
